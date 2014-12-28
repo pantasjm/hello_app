032f0055ecac1e52449f952c4c8501e8ce2e2298
Jose Miguel <pantasjm@gmail.com>
2014-12-28 00:39:26 +0000

    Cambio en el mensaje

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "Hola Alvarito!!"
+    render text: "Hola mama! Estoy haciendo mi primera aplicacion en linea!! Y la puedes ver desde cualquier aparato!!"
   end
   
 end
