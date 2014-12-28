c1f974409ac6d756d38aea5fec17f98596591a52
Jose Miguel <pantasjm@gmail.com>
2014-12-28 01:33:24 +0000

    Hola Mundo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
+# encoding: utf-8
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
   def hello
-    render text: "Hola amor! Estoy haciendo mi primera aplicacion en linea!! Y la puedes ver desde cualquier aparato!!"
+    render text: "¡Hola, mundo!"
   end
   
 end
